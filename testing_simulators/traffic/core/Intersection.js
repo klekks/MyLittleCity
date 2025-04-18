4466b2f4cb80e0e8472d2b435ef439440d928040
Ilya Petrov <klekks@ya.ru>
2025-04-18 18:50:30 +0300

    refactoring Intersections: begin

--- testing_simulators/traffic/core/Intersection.js
+++ testing_simulators/traffic/core/Intersection.js
@@ -2,29 +2,56 @@
 const MAX_CONNECTIONS_PER_INTERSECTION = 4;
 
 export class Intersection {
-  constructor(x, y) {
-      this.x = x;
-      this.y = y;
-      this.connectedRoads = [];
-  }
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.connectedRoads = [];
+    }
 
-  canConnectMore() {
-      return this.connectedRoads.length < MAX_CONNECTIONS_PER_INTERSECTION;
-  }
+    radius()
+    {
+        return this.connectedRoads.length <= 2 ? 10 : 14;
+    }
 
-  connectRoad(road) {
-      if (this.connectedRoads.includes(road)) return;
-      if (this.canConnectMore()) {
-          this.connectedRoads.push(road);
-      } else {
-          throw new Error("Intersection at (" + this.x + ", " + this.y + ") has reached max connections.");
-      }
-  }
+    canConnectMore() 
+    {
+        return this.connectedRoads.length < MAX_CONNECTIONS_PER_INTERSECTION;
+    }
 
-  disconnectRoad(road) {
-      const index = this.connectedRoads.indexOf(road);
-      if (index !== -1) {
-          this.connectedRoads.splice(index, 1);
-      }
-  }
+    connectRoad(road) 
+    {
+        if (this.connectedRoads.includes(road)) 
+            return;
+        if (this.canConnectMore()) 
+        {
+            this.connectedRoads.push(road);
+        } 
+        else 
+        {
+            throw new Error("Intersection at (" + this.x + ", " + this.y + ") has reached max connections.");
+        }
+    }
+
+    disconnectRoad(road) 
+    {
+        const index = this.connectedRoads.indexOf(road);
+        if (index !== -1) 
+        {
+            this.connectedRoads.splice(index, 1);
+        }
+    }
+
+    getConnectedLanes()
+    {
+        let lanes = [];
+        for (let road of this.connectedRoads)
+        {
+            lanes = [...lanes, ...road.getLanes()];
+        }
+        return lanes;
+    }
+
+    // TODO: get out Lanes
+    // TODO: get in Lanes
+    // TODO: getLanesAvailableFrom
 }
--- testing_simulators/traffic/graphics/IntersectionView.js
+++ testing_simulators/traffic/graphics/IntersectionView.js
@@ -1,37 +1,37 @@
 import { Intersection } from '../core/Intersection.js';
 
-export class IntersectionView extends Intersection {
-  draw(ctx) {
-    const numRoads = this.connectedRoads.length;
-    const size = numRoads <= 2 ? 10 : 14;
+export class IntersectionView extends Intersection 
+{
+    draw(ctx) 
+    {
+        const size = this.radius();
 
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, size, 0, Math.PI * 2);
-    ctx.fillStyle = "#444";
-    ctx.fill();
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, size, 0, Math.PI * 2);
+        ctx.fillStyle = "#444";
+        ctx.fill();
 
-    // разметка: белые лучи в направлении каждой дороги
-    for (const road of this.connectedRoads) {
-      const other =
-        road.startIntersection === this ? road.endIntersection : road.startIntersection;
-      const dx = other.x - this.x;
-      const dy = other.y - this.y;
-      const len = Math.hypot(dx, dy);
-      const normX = dx / len;
-      const normY = dy / len;
+        // разметка: белые лучи в направлении каждой дороги
+        for (const road of this.connectedRoads) {
+            const other_interseption_for_this_road = road.startIntersection === this ? road.endIntersection : road.startIntersection;
+            const dx = other_interseption_for_this_road.x - this.x;
+            const dy = other_interseption_for_this_road.y - this.y;
+            const len = Math.hypot(dx, dy);
+            const normX = dx / len;
+            const normY = dy / len;
 
-      const lineLength = 16;
-      const startX = this.x + normX * 4;
-      const startY = this.y + normY * 4;
-      const endX = this.x + normX * lineLength;
-      const endY = this.y + normY * lineLength;
+            const lineLength = 16;
+            const startX = this.x + normX * 4;
+            const startY = this.y + normY * 4;
+            const endX = this.x + normX * lineLength;
+            const endY = this.y + normY * lineLength;
 
-      ctx.beginPath();
-      ctx.moveTo(startX, startY);
-      ctx.lineTo(endX, endY);
-      ctx.strokeStyle = "#fff";
-      ctx.lineWidth = 2;
-      ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(startX, startY);
+            ctx.lineTo(endX, endY);
+            ctx.strokeStyle = "#fff";
+            ctx.lineWidth = 2;
+            ctx.stroke();
+        }
     }
-  }
 }
